01e03bfeae92f59bfe5cdfda437e02448f76aa8f
rahdi <adrian.heiden@gmail.com>
2023-08-21 13:24:09 +0200

    fixed bug with unnecessary fetching of recipes after editing one

--- src/app/recipe-book/recipes-resolver.service.ts
+++ src/app/recipe-book/recipes-resolver.service.ts
@@ -7,15 +7,24 @@ import {
 import { Recipe } from './models';
 import { DataStorageService } from '../shared';
 import { Observable } from 'rxjs';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this.dataStorageService.fetchRecipes();
+    const existingRecipes = this.recipeService.getRecipes();
+
+    if (existingRecipes.length === 0)
+      return this.dataStorageService.fetchRecipes();
+
+    return existingRecipes;
   }
 }
