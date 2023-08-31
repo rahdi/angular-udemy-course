33f305cf887958a3e013f371c47bd44b7324fcdc
rahdi <adrian.heiden@gmail.com>
2023-08-31 12:59:16 +0200

    added router-store and store-devtools to display events in redux-devtools in browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared';
@@ -10,8 +13,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from './environments/environment';
 // import { LoggingService } from './logging.service';
 
 @NgModule({
@@ -24,6 +27,8 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppRoutingModule, // this should be last to enable proper recipes routes. The routing arrays are concatenated in the order given here
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   // providers: [LoggingService],
   bootstrap: [AppComponent],
